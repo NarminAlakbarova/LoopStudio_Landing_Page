93497943ffff16da1cec5d42757b05330ef9894f
NarminAlakbarova <735e5lb@code.edu.az>
2023-05-15 22:55:27 +0400

    update:js and responsiveness

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-let icon = document.querySelector(".iconmenu");
-let nav2 = document.querySelector(".nav2");
-let nav1 = document.querySelector(".desktop-nav");
-let value = false;
-icon.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (!value) {
-    nav1.style.display = "none";
-    nav2.style.display = "block";
-    nav2.style.backgroundColor = "black";
-    value=true
-  } else {
-    nav1.style.display = "block";
-    nav2.style.display = "none";
-    value=false
+let icon = document.querySelector(".fa-bars");
+let nav = document.querySelector("nav");
+icon.addEventListener("click",function(){
+  nav.classList.toggle("show");
 
-  }
-});
+  console.log("hello");
+})
